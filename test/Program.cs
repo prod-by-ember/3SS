dd44a3a26efb89e54c6f10a8682b2f7c6b700906
ember <luxunderscoremusic@gmail.com>
2024-05-29 13:54:05 -0400

    Framework

--- test/Program.cs
+++ test/Program.cs
@@ -5,9 +5,29 @@ namespace test;
 
 class Program
 {
+	private static string comkey = "A1A0B1B1";
+	private static int[,] matrix = { { 12, 19 }, { 13, 18 } };
+	private static int[] tzc = { 8, 1, -4, -5, 2 };
+	private static int tzcode = tzc[(int)Char.ToUpper(comkey[2]) - 65] + (comkey[3] == '1' ? 1 : 0);
+
+	private static string t = "abcdefghijklmnopqrstuvwxyz ,.";
+
 	static void Main(string[] args)
 	{
-		Console.WriteLine(DateTime.UtcNow.Hour);
+		Console.WriteLine(DateTime.UtcNow.Second);
+		Console.WriteLine((DateTime.UtcNow.Hour + tzcode) % 24);
+		Console.WriteLine(AF("Hello, world."));
 	}
-
+	static string AF(string p)
+	{
+		int a = Convert.ToInt32(Convert.ToString(DateTime.UtcNow.Second).Substring(1)) == 0 ? 1 : Convert.ToInt32(Convert.ToString(DateTime.UtcNow.Second).Substring(1));
+		int b = (DateTime.UtcNow.Hour + tzcode) % 24;
+		string c = "";
+		for (int i = 0; i < p.Length; i++)
+		{
+			int n = (t.IndexOf(Char.ToLower(p[i])) * a + b) % 29;
+			c += Char.IsLower(p[i]) || !t.ToUpper().Substring(0, 26).Contains(p[i]) ? t[n] : Char.ToUpper(t[n]);
+		}
+		return c;
+	}
 }
--- test/obj/Debug/net8.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f125d5eae49048e565533d0e09f9ad8f0d02cd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ed43713ec313e7fc38ded3f1768131a2a939c66")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
