425fe717b87429beb42e4c858d9e5bda4ca5a6d2
ember <luxunderscoremusic@gmail.com>
2024-05-29 12:09:59 -0400

    Framework

--- .3ss/main.cs
+++ .3ss/main.cs
@@ -2,7 +2,17 @@
 
 public class encode
 {
-
+	// Step 3
+	static string AF(string p, int a, int b)
+	{
+		string c = "";
+		for (int i = 0; i < p.Length; i++)
+		{
+			int n = (t.IndexOf(Char.ToLower(p[i])) * a + b) % 29;
+			c += Char.IsLower(p[i]) || !t.ToUpper().Substring(0, 26).Contains(p[i]) ? t[n] : Char.ToUpper(t[n]);
+		}
+		return c;
+	}
 }
 public class decode
 {
--- test/Program.cs
+++ test/Program.cs
@@ -7,7 +7,6 @@ class Program
 	private static string t = "abcdefghijklmnopqrstuvwxyz ,.";
 	static void Main(string[] args)
 	{
-		string comKey = "A1A0B1B1";
 	}
 	static string HI()
 	{
@@ -16,15 +15,5 @@ class Program
 	static string RF()
 	{
 
-	}
-	static string AF(string p, int a, int b)
-	{
-		string c = "";
-		for (int i = 0; i < p.Length; i++)
-		{
-			int n = (t.IndexOf(Char.ToLower(p[i])) * a + b) % 29;
-			c += Char.IsLower(p[i]) || !t.ToUpper().Substring(0, 26).Contains(p[i]) ? t[n] : Char.ToUpper(t[n]);
-		}
-		return c;
 	}
 }
